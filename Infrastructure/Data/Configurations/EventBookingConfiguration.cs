ffde25e19beee8e4326bdf2f6651e4bb6b20f92f
kadirovaMadina <madina.80008@gmail.com>
2024-04-15 11:53:22 +0500

    Configurations_Siyavush

--- Infrastructure/Data/Configurations/EventBookingConfiguration.cs
+++ Infrastructure/Data/Configurations/EventBookingConfiguration.cs
@@ -1,21 +1,36 @@
-using Microsoft.EntityFrameworkCore;
+using Domain.Models;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Infrastructure.Models; 
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Infrastructure.Data.Configurations 
+namespace Persistance.Configuration
 {
-	public class EventBookingConfiguration : IEntityTypeConfiguration<EventBooking>
-	{
-		public void Configure(EntityTypeBuilder<EventBooking> builder)
-		{
-			builder.HasKey(eb => eb.Id);
-			builder.Property(eb => eb.EventId).IsRequired();
-			builder.Property(eb => eb.ParticipantId);
-			builder.Property(eb => eb.BookingDate);
-			builder.Property(eb => eb.SeatId);
-			builder.Property(eb => eb.CreatedDate);
-			builder.Property(eb => eb.UpdatedDate);
-			builder.HasOne(eb => eb.Payment).WithOne().HasForeignKey<EventBooking>(eb => eb.PaymentId);
-		}
-	}
+    public class EventBookingConfiguration : IEntityTypeConfiguration<EventBooking>
+    {
+        public void Configure(EntityTypeBuilder<EventBooking> builder)
+        {
+            builder.HasKey(eb => eb.Id);
+            builder.Property(eb => eb.EventId).IsRequired();
+            builder.Property(eb => eb.ParticipantId);
+            builder.Property(eb => eb.BookingDate);
+            builder.Property(eb => eb.SeatId);
+            builder.Property(eb => eb.CreatedDateTime);
+            builder.Property(eb => eb.UpdatedDateTime);
+            builder.HasOne(eb => eb.Payment).WithOne().HasForeignKey<EventBooking>(eb => eb.PaymentId);
+
+            builder.HasOne(eb => eb.Seat)
+            .WithMany(s => s.EventBookings)
+            .HasForeignKey(eb => eb.SeatId);
+
+            builder.HasOne(eb => eb.Ticket)
+                .WithOne(t => t.EventBooking)
+                .HasForeignKey(eb => eb.TicketId);
+
+
+        }
+    }
 }
